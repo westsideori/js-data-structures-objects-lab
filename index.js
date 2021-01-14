cc433316f2cbeec3d79a994a7b9d87159b62da32
Ori Markowitz <ori.markowitz@gmail.com>
2021-01-13 23:04:54 -0500

    commit tree 23f2c74789224215027e93c8d99929a837fb2305

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const driver = {};
+
+const updateDriverWithKeyAndValue = (driver, key, value) => {
+    const newDriver = Object.assign({}, driver, {[key]:value});
+    return newDriver  
+};
+
+const destructivelyUpdateDriverWithKeyAndValue = (driver, key, value) => {
+    driver[key] = value;
+    return driver;
+};
+
+const deleteFromDriverByKey = (driver, key) => {
+    const newUpdatedDriver = Object.assign({}, driver);
+    delete newUpdatedDriver[key];
+    return newUpdatedDriver;
+};
+
+const destructivelyDeleteFromDriverByKey = (driver, key) => {
+    delete driver[key];
+    return driver;
+};
